81714b88dd02d12392a0f4323b416c4ba27ed8f6
husseng96 <97613314+husseng96@users.noreply.github.com>
2023-04-30 21:16:59 -0400

    js class
    read data in  .csv

---
+++ script.js
@@ -0,0 +1,13 @@
+/* global d3, Promise */
+
+(function() { 
+    // Read data from the CSV files
+    Promise.all([
+        d3.csv("overall.csv"),
+        d3.csv("la_liga.csv"),
+        d3.csv("national.csv"),
+        d3.csv("yearly_goals.csv")
+
+    ])
+
+    })();
